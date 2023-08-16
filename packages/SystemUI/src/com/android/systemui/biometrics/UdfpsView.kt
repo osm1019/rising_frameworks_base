fd9facefa8b8ba22965c2fb3fec8871c152dfa90
osm1019 <osm1019@gmail.com>
2023-08-16 13:44:05 -0700

    Revert "udfps: Restore illumination dot for global hbm"
    This reverts commit 5193f41beb67900435ce47c653463f0c7d6317ca.
    
    Change-Id: I81941e173038f1a8ed222e16deeed01c732b3f7f

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.kt
@@ -25,7 +25,6 @@ import android.graphics.RectF
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
-import android.view.Surface
 import android.widget.FrameLayout
 import com.android.systemui.R
 import com.android.systemui.doze.DozeReceiver
@@ -61,8 +60,6 @@ class UdfpsView(
             a.getFloat(R.styleable.UdfpsView_sensorTouchAreaCoefficient, 0f)
         }
 
-    private var ghbmView: UdfpsSurfaceView? = null
-
     /** View controller (can be different for enrollment, BiometricPrompt, Keyguard, etc.). */
     var animationViewController: UdfpsAnimationViewController<*>? = null
 
@@ -87,10 +84,6 @@ class UdfpsView(
     // Don't propagate any touch events to the child views.
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
         return (animationViewController == null || !animationViewController!!.shouldPauseAuth())
-    }
-
-    override fun onFinishInflate() {
-        ghbmView = findViewById(R.id.hbm_view)
     }
 
     override fun dozeTimeTick() {
@@ -175,15 +168,12 @@ class UdfpsView(
             onDisplayConfigured?.run()
             ghbmView?.drawIlluminationDot(RectF(sensorRect))
         }
+        mUdfpsDisplayMode?.enable(onDisplayConfigured)
     }
 
     fun unconfigureDisplay() {
         isDisplayConfigured = false
         animationViewController?.onDisplayUnconfigured()
-        ghbmView?.let { view ->
-            view.setGhbmIlluminationListener(null)
-            view.visibility = INVISIBLE
-        }
         mUdfpsDisplayMode?.disable(null /* onDisabled */)
     }
 }
